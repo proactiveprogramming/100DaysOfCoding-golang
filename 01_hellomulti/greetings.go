cdda9e689190ba988ed36579fcc88b85964dda62
Melissa Janine Newman <51647658+proactiveprogramming@users.noreply.github.com>
2022-06-02 11:22:13 +0300

    Initial file.

---
+++ 01_hellomulti/greetings.go
@@ -0,0 +1,14 @@
+// File: greetings/greetings.go
+// Version: 0.1
+// -------------------------------------------------------
+// Purpose: To create a greetings package.
+package greetings
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    return message
+}
