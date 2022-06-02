e7fc21eb9860165d13e655f4c24ddfda51e512a0
Melissa Janine Newman <51647658+proactiveprogramming@users.noreply.github.com>
2022-06-02 09:40:34 +0300

    Initial file

---
+++ golangsetup.sh
@@ -0,0 +1,13 @@
+// File: golangsetup.sh
+// Original Author: Melissa Neman (Proactive Programming)
+// Purpose: Run once to setup the golang environement.  Also add the code in bashrc to the ~/.bashrc file.
+//
+// the "locahost" directory is where I stored my locally developed code that is not connected to github, gitlab, or another online git location.
+
+mkdir $HOME/Projects
+mkdir $HOME/Projects/goprojects
+mkdir $HOME/Projects/goprojects/bin
+mkdir $HOME/Projects/goprojects/pkg
+mkdir $HOME/Projects/goprojects/src
+mkdir $HOME/Projects/goprojects/src/localhost
+
